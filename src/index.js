2d45e78dc87c99d629355ebcee259d21cb505c56
fedoroff94 <artsemfedziankou@gmail.com>
2021-03-11 22:49:24 +0300

    feat: delete index.html

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-
     let bracketsMap = {},
         openingBrackets = new Set(),
         closingBrackets = new Set();
-
     bracketsConfig.forEach(pair => {
         bracketsMap[pair[1]] = pair[0];
         openingBrackets.add(pair[0]);
         closingBrackets.add(pair[1]);
     });
-
     let stack = [];
-
     for (let i = 0; i < str.length; i++) {
         if (openingBrackets.has(str[i])) {
             if (str[i] === stack[stack.length - 1] && closingBrackets.has(str[i])) {
